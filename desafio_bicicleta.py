b4bd8fd3cee9bb33775de104f342d59d08f3d464
SidneySena <sid-sena@hotmail.com>
2025-08-12 15:38:12 -0300

    desafios Python

---
+++ desafio_bicicleta.py
@@ -0,0 +1,20 @@
+class bicicleta:
+    def __init__(self,cor,modelo,ano,valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+    def buzinar (self):
+        print ("plim plim ...")
+    def parar (self):
+        print ("parando bicicleta")
+        print ("bicicleta parada!")
+    def correr (self):
+        print ("vruuuuuum....")
+    def __str__(self):
+        return f"{self.__class__.__name__}:{' ,'.join([f'{chave}={valor}'for chave , valor in self.__dict__.items()])}"
+b1 = bicicleta("veremelha", "Caloi",2022,600)
+b1.buzinar()
+b1.correr()
+b1.parar()
+print(b1.cor,b1.modelo,b1.ano,b1.valor)
